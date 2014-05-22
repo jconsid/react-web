7441ecee67676d22335da8968969557e3c7004ba
swanmo <swanmo@gmail.com>
2014-05-22 21:39:05 +0200

    Tog bort ping-sidan.

--- web/app/js/app.js
+++ web/app/js/app.js
@@ -19,6 +19,5 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/loglist', {templateUrl: 'partials/loglist.html', controller: 'AdminLogCtrl'});
   $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'LoginCtrl'});
   $routeProvider.when('/ticket/:ticketId', {templateUrl: 'partials/ticket.html', controller: 'TicketCtrl'});
-  $routeProvider.when('/ping', {templateUrl: 'partials/ping.html', controller: 'PingCtrl'});
   $routeProvider.otherwise({redirectTo: '/list'});
 }]);
