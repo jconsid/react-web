9f1023d03921200e070047b35d431a8a5a02592c
swanmo <swanmo@gmail.com>
2014-07-31 01:40:19 +0200

    Rensade onödiga tester

--- web/test/unit/servicesSpec.js
+++ web/test/unit/servicesSpec.js
@@ -6,11 +6,5 @@ describe('service', function() {
   beforeEach(module('myApp.services'));
 
 
-  describe('version', function() {
-    it('should return current version', inject(function(version) {
-      expect(version).toEqual('0.1');
-    }));
-  });
-
 
 });
