1e236eb44f35eef7d0794d5e01be47ef37167e48
swanmo <swanmo@gmail.com>
2014-08-05 00:59:34 +0200

    La till datepicker.
    - Datepicker på register.html.
    - https://github.com/adamalbrecht/ngQuickDate

--- web/app/js/app.js
+++ web/app/js/app.js
@@ -6,18 +6,29 @@
     'poa.services',
     'poa.controllers',
     'ngCookies',
-    'ngAnimate'
+    'ngAnimate',
+    'ngQuickDate'
   ]).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/list', {templateUrl: 'partials/list.html', controller: 'ListCtrl as main'});
-    $routeProvider.when('/register', {templateUrl: 'partials/register.html', controller: 'RegisterCtrl'});
-    $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'LoginCtrl'});
-    $routeProvider.when('/editera/:anmalanId', {templateUrl: 'partials/register.html', controller: 'EditAnmalanCtrl'});
-    $routeProvider.when('/anmalan/:anmalanId', {templateUrl: 'partials/anmalan.html', controller: 'AnmalanCtrl as viewCtrl'});
-    $routeProvider.when('/om', {templateUrl: 'partials/about.html', controller: 'AboutCtrl'});
-    $routeProvider.otherwise({redirectTo: '/list'});
-  }]);
+  config(['$routeProvider',
+    function($routeProvider) {
+      $routeProvider.when('/list', {templateUrl: 'partials/list.html', controller: 'ListCtrl as main'});
+      $routeProvider.when('/register', {templateUrl: 'partials/register.html', controller: 'RegisterCtrl'});
+      $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'LoginCtrl'});
+      $routeProvider.when('/editera/:anmalanId', {templateUrl: 'partials/register.html', controller: 'EditAnmalanCtrl'});
+      $routeProvider.when('/anmalan/:anmalanId', {templateUrl: 'partials/anmalan.html', controller: 'AnmalanCtrl as viewCtrl'});
+      $routeProvider.when('/om', {templateUrl: 'partials/about.html', controller: 'AboutCtrl'});
+      $routeProvider.otherwise({redirectTo: '/list'});
 
+    }]).
+  config(function(ngQuickDateDefaultsProvider) {
+      return ngQuickDateDefaultsProvider.set({
+        dayAbbreviations: ["Sö", "Må", "Ti", "On", "To", "Fr", "Lö"],
+        nextLinkHtml: 'Nästa &rarr;',
+        prevLinkHtml: '&larr; Tidigare',
+        dateFormat: 'yyyy-MM-dd',
+        timeFormat: 'hh:mm'
+      });
+    });
   angular.module('poa.services', []);
   angular.module('poa.controllers', []);
 })();
